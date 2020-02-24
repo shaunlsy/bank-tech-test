4e93274847e68d9e860cf136a1d0c59282c68e35
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-02-24 14:50:12 +0000

    - run rubocop --init - exclude spec line length in rubocop.yml - pass rubocop test

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,4 +1,8 @@
+# frozen_string_literal: true
+
 require 'time'
+
+# this is bank
 class Bank
   attr_reader :balance, :date, :statement
 
@@ -10,25 +14,27 @@ class Bank
   def deposit(amount, date = Time.now.to_s)
     @balance += amount
     @date = Time.parse(date).strftime('%d/%m/%Y')
-    @statement << {date: @date, credit: amount, balance: @balance}
+    @statement << { date: @date, credit: amount, balance: @balance }
   end
 
   def withdraw(amount, date = Time.now.to_s)
-    raise "Balance not enough for you to withdraw" if @balance - amount < 0
+    raise 'Balance not enough to withdraw' if (@balance - amount).negative?
+
     @balance -= amount
     @date = Time.parse(date).strftime('%d/%m/%Y')
-    @statement << {date: @date, debit: amount, balance: @balance}
+    @statement << { date: @date, debit: amount, balance: @balance }
   end
 
   def print_statement
-    puts "date || credit || debit || balance"
+    puts 'date || credit || debit || balance'
     @statement.reverse.each do |transaction|
       if transaction.include?(:credit)
-        puts "#{transaction[:date]} || || %.2f || %.2f" % [transaction[:credit], transaction[:balance]]
-      else 
-        puts "#{transaction[:date]} || %.2f || || %.2f" % [transaction[:debit], transaction[:balance]]
+        puts format("#{transaction[:date]} || || %.2f || %.2f",
+                    transaction[:credit], transaction[:balance])
+      else
+        puts format("#{transaction[:date]} || %.2f || || %.2f",
+                    transaction[:debit], transaction[:balance])
       end
     end
   end
-
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'bank'
 
 describe Bank do
@@ -8,26 +10,26 @@ describe Bank do
   end
 
   it 'adds money into the balance' do
-    subject.deposit(100, "14/01/2012")
+    subject.deposit(100, '14/01/2012')
     expect(subject.balance).to eq 100
   end
 
   describe '#withdraw' do
     it 'removes money from the balance' do
-      subject.deposit(10000, "14/01/2012")
-      subject.withdraw(1, "15/01/2012")
+      subject.deposit(10_000, '14/01/2012')
+      subject.withdraw(1, '15/01/2012')
       expect(subject.balance).to eq 9999
     end
 
     it 'raises an error if there is not enough money to be withdrawn' do
-      expect{ subject.withdraw(50000) }.to raise_error "Balance not enough for you to withdraw"
+      expect { subject.withdraw(50_000) }.to raise_error 'Balance not enough to withdraw'
     end
   end
 
   it 'prints out bank statement' do
-    subject.deposit(2000, "14/01/2012")
-    subject.withdraw(1000, "15/01/2012")
-    expect{ subject.print_statement }.to output("date || credit || debit || balance\n15/01/2012 || 1000.00 || || 1000.00\n14/01/2012 || || 2000.00 || 2000.00\n").to_stdout
+    subject.deposit(2000, '14/01/2012')
+    subject.withdraw(1000, '15/01/2012')
+    statement = "date || credit || debit || balance\n15/01/2012 || 1000.00 || || 1000.00\n14/01/2012 || || 2000.00 || 2000.00\n"
+    expect { subject.print_statement }.to output(statement).to_stdout
   end
-
 end
